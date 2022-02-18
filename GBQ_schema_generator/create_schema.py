8e7bfe110dd72464dd53d4396fe59ce0106d2903
puunyapat-convz <puunyapat@convz.com>
2022-02-18 19:25:23 +0700

    Update mapping logic Update schema file

--- GBQ_schema_generator/create_schema.py
+++ GBQ_schema_generator/create_schema.py
@@ -4,6 +4,29 @@ import json, os, shutil
 # set path to script location
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+BQ_DTYPE = [
+    [ "STRING", "char", "nchar", "nvarchar", "varchar", "sysname", "text", "uniqueidentifier" ],
+    [ "INTEGER", "int", "tinyint", "smallint", "bigint" ],
+    [ "FLOAT", "numeric", "decimal", "money" ],
+    [ "BOOLEAN", "bit", "boolean" ],
+    [ "DATE", "date" ],
+    [ "TIME", "time" ],
+    [ "DATETIME", "datetime", "datetime2", "smalldatetime" ],
+    [ "TIMESTAMP", "timestamp" ]
+]
+
+PD_DTYPE = {
+    "INTEGER"  : "int64",
+    "STRING"   : "object",
+    "FLOAT"    : "float64",
+    "BOOLEAN"  : "bool",
+    "DATE"     : "datetime64",
+    "TIME"     : "datetime64",
+    "DATETIME" : "datetime64",
+    "TIMESTAMP": "datetime64"
+}
+
+
 def mkDir(path,jump):
     if not os.path.exists(path):
         os.makedirs(path)
@@ -11,30 +34,18 @@ def mkDir(path,jump):
         os.chdir(path)
 
 def convertType(data_type):
-    type_table = {                     
-                    "char"          : "STRING",
-                    "nchar"         : "STRING",
-                    "nvarchar"      : "STRING",
-                    "varchar"       : "STRING",
-                    "sysname"       : "STRING",
-                    "bigint"        : "INTEGER",
-                    "smallint"      : "INTEGER",
-                    "tinyint"       : "INTEGER",
-                    "int"           : "INTEGER",
-                    "numeric"       : "FLOAT",
-                    "decimal"       : "FLOAT",
-                    "money"         : "FLOAT",
-                    "date"          : "DATE",
-                    "datetime2"     : "DATETIME",
-                    "smalldatetime" : "DATETIME"
-                    "timestamp"     : "TIMESTAMP"
-                 } 
-    return type_table.get(data_type, data_type)
+
+    for line in BQ_DTYPE:
+        if data_type.strip() in line:
+            gbq_data_type = line[0]
+            break
+
+    return gbq_data_type
 
 # read Excel file
-my_sheet = 'Field-Officemate'
+my_sheet = 'Field-MDS'
 file_name = 'OFM-B2S_Source_Datalake_20211020-live-version.xlsx'
-df = pd.read_excel(file_name, sheet_name = my_sheet, usecols = [0,1,2,3])
+df = pd.read_excel(file_name, sheet_name = my_sheet, usecols = ["Source","Table_Name","Field_Name","Field_Type","IS_NULLABLE"])
 
 mkDir("schemas",True)
 
@@ -50,7 +61,8 @@ mkDir(source_name,True)
 print(f"Converting source: {source_name}")
 
 exclude_name = []
-all_excel_tb = df.iloc[:,1].unique()
+all_excel_tb = [ "V_PROD_CLUSTER_2" ]
+# all_excel_tb = df.iloc[:,1].unique()
 
 for table_name in all_excel_tb:
     
@@ -65,9 +77,9 @@ for table_name in all_excel_tb:
         #print(tables_fields.iloc[:,[2,3]])
 
         for row in tables_fields.itertuples(index=True, name='Pandas'):
-            #print(row[2],row[3],row[4])
+            gbq_field_mode = "REQUIRED" if str(row[5]) in [ "0", "0.0", "NO" ] else "NULLABLE"
             new_type = convertType(str(row[4]).lower())
-            all_fields.append({"mode":"NULLABLE", "name":str(row[3]), "type":new_type.upper()})
+            all_fields.append({"mode":gbq_field_mode, "name":str(row[3]), "type":new_type.upper()})
         
         #print(all_fields)
         #print()
