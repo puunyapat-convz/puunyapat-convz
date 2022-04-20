4f69a5e01ebdcfa45e9ec8064f71c9293117d32b
puunyapat-convz <puunyapat@convz.com>
2022-04-20 13:18:00 +0700

    update logic for intraday

--- daily_gcs2gbq/gcs2gbq_create_var.py
+++ daily_gcs2gbq/gcs2gbq_create_var.py
@@ -13,31 +13,30 @@ log       = logging.getLogger(__name__)
 path      = configuration.get('core','dags_folder')
 MAIN_PATH = path + "/../data"
 
-PROJECT_ID   = "central-cto-ofm-data-hub-dev"
-DATASET_ID   = "test_airflow"
-LOCATION     = "asia-southeast1" 
-
 BUCKET_NAME  = "ofm-data"
 SOURCE_NAME  = [ "ERP", "MDS", "officemate" ] 
-SOURCE_TYPE  = "daily"
+SOURCE_TYPE  = { "daily" : "| egrep -iv '^$|^erp_'", "intraday": "" }
 
 ###############################
 
-def _read_table(source, filename):
+def _read_table(name, type, filename):
     with open(filename) as f:
         lines = f.read().splitlines()
 
-    Variable.set(
-        key   = f'{source}_tables',
-        value = lines,
-        serialize_json = True
-    )
+    if len(lines) > 0:
+        Variable.set(
+            key   = f'{name}_{type}',
+            value = lines,
+            serialize_json = True
+        )
+    else:
+        log.info(f"No folders in gs://{BUCKET_NAME}/{name}/{type}")
 
 with DAG(
     dag_id="gcs2gbq_create_var",
     # schedule_interval="00 00 * * *",
     schedule_interval=None,
-    start_date=dt.datetime(2022, 3, 28),
+    start_date=dt.datetime(2022, 4, 15),
     catchup=False,
     tags=['convz_prod_airflow_style']
 ) as dag:
@@ -45,32 +44,34 @@ with DAG(
     start_task = DummyOperator(task_id = "start_task")
     end_task = DummyOperator(task_id = "end_task")
 
-    for SOURCE in SOURCE_NAME:
+    for NAME in SOURCE_NAME:
+        for TYPE in SOURCE_TYPE.keys():
 
-        get_table_names = BashOperator(
-            task_id  = f"get_{SOURCE}_tables",
-            cwd      = MAIN_PATH,
-            bash_command = f"gsutil ls gs://{BUCKET_NAME}/{SOURCE}/{SOURCE_TYPE}"
-                            + f" | grep -v erp_ | sed '1d' | cut -d'/' -f6 > {SOURCE}_tm1_folders; cat {SOURCE}_tm1_folders;"
-                            + f" echo {MAIN_PATH}/{SOURCE}_tm1_folders"
-        )
+            get_table_names = BashOperator(
+                task_id  = f"get_{NAME}_{TYPE}",
+                cwd      = MAIN_PATH,
+                bash_command = f"gsutil ls gs://{BUCKET_NAME}/{NAME}/{TYPE}" \
+                                + f" | cut -d'/' -f6 {SOURCE_TYPE.get(TYPE)} > {NAME}_{TYPE}_folders;" \
+                                + f" echo {MAIN_PATH}/{NAME}_{TYPE}_folders"
+            )
 
-        read_table_list = PythonOperator(
-            task_id = f"read_{SOURCE}_list",
-            python_callable = _read_table,
-            op_kwargs = {
-                'source'   : SOURCE,
-                'filename' : f'{{{{ ti.xcom_pull(task_ids="get_{SOURCE}_tables") }}}}',
-                # 'filename' : '/Users/oH/airflow/dags/ERP_tm1_folders'
-            },
-        )
+            read_table_list = PythonOperator(
+                task_id = f"read_{NAME}_{TYPE}",
+                python_callable = _read_table,
+                op_kwargs = {
+                    'name' : NAME,
+                    'type' : TYPE,
+                    'filename' : f'{{{{ ti.xcom_pull(task_ids="get_{NAME}_{TYPE}") }}}}',
+                    # 'filename' : '/Users/oH/airflow/dags/ERP_tm1_folders'
+                },
+            )
 
-        remove_table_list = BashOperator(
-            task_id  = f"remove_{SOURCE}_list",
-            cwd      = MAIN_PATH,
-            trigger_rule = 'all_done',
-            bash_command = f"rm -f {{{{ ti.xcom_pull(task_ids='get_{SOURCE}_tables') }}}}"
-        )
+            remove_table_list = BashOperator(
+                task_id  = f"remove_{NAME}_{TYPE}",
+                cwd      = MAIN_PATH,
+                trigger_rule = 'all_done',
+                bash_command = f"rm -f {{{{ ti.xcom_pull(task_ids='get_{NAME}_{TYPE}') }}}}"
+            )
 
-        ## loop level dependencies
-        start_task >> get_table_names >> read_table_list >> remove_table_list >> end_task
+            ## loop level dependencies
+            start_task >> get_table_names >> read_table_list >> remove_table_list >> end_task
