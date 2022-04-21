c573b48d2b39525ff05e067ff76217c093ef7c96
puunyapat-convz <puunyapat@convz.com>
2022-04-21 11:59:24 +0700

    add default_args

--- migrate2prod/migrate_erp.py
+++ migrate2prod/migrate_erp.py
@@ -199,6 +199,26 @@ with DAG(
     catchup=False,
     tags=['convz_prod_migration'],
     render_template_as_native_obj=True,
+    # default_args={
+    #     'retries': 1,
+    #     'retry_delay': dt.timedelta(seconds=5),
+    #     'depends_on_past': False,
+    #     'email': ['airflow@example.com'],
+    #     'email_on_failure': False,
+    #     'email_on_retry': False,
+    #     'queue': 'bash_queue',
+    #     'pool': 'backfill',
+    #     'priority_weight': 10,
+    #     'end_date': datetime(2016, 1, 1),
+    #     'wait_for_downstream': False,
+    #     'sla': timedelta(hours=2),
+    #     'execution_timeout': timedelta(seconds=300),
+    #     'on_failure_callback': some_function,
+    #     'on_success_callback': some_other_function,
+    #     'on_retry_callback': another_function,
+    #     'sla_miss_callback': yet_another_function,
+    #     'trigger_rule': 'all_success'
+    # },
 ) as dag:
 
     start_task = DummyOperator(task_id = "start_task")
