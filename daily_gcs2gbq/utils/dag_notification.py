7b1e3d066a06e5000d533ad38923f22cc6f9dc0a
puunyapat-convz <puunyapat@convz.com>
2022-04-26 18:26:25 +0700

    add missing file alert

--- daily_gcs2gbq/utils/dag_notification.py
+++ daily_gcs2gbq/utils/dag_notification.py
@@ -2,11 +2,12 @@ from airflow.hooks.base_hook import BaseHook
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 
 SLACK_OFM_ALERT_CONN = 'slack_ofm_alert_conn'
-
+slack_webhook_token  = BaseHook.get_connection(SLACK_OFM_ALERT_CONN).password
 
 def ofm_task_fail_slack_alert(context):
-    slack_webhook_token = BaseHook.get_connection(
-        SLACK_OFM_ALERT_CONN).password
+
+    ti = context.get('task_instance')
+    
     slack_msg = """
             OFM alert: Task Failed. 
             Task: {task}  
@@ -14,14 +15,48 @@ def ofm_task_fail_slack_alert(context):
             Execution Time: {exec_date}  
             Log Url: {log_url} 
             """.format(
-        task=context.get('task_instance').task_id,
-        dag=context.get('task_instance').dag_id,
-        exec_date=context.get('execution_date'),
-        log_url=context.get('task_instance').log_url
-    )
+                task = ti.task_id,
+                dag  = ti.dag_id,
+                log_url   = ti.log_url,
+                exec_date = context.get('ts')
+            )
+
     failed_alert = SlackWebhookOperator(
         task_id='slack_test',
         http_conn_id=SLACK_OFM_ALERT_CONN,
         webhook_token=slack_webhook_token,
-        message=slack_msg)
+        message=slack_msg
+    )
+
+    return failed_alert.execute(context=context)
+
+def ofm_missing_file_slack_alert(context):
+
+    ti = context.get('task_instance')
+    ts = context.get('ts')
+    table_name = ti.task_id.split('_')[-1]
+    gcs_list = ti.xcom_pull(key='gcs_uri', task_ids=f'check_tm1_list_{table_name}').split('/')
+
+    slack_msg = """
+                ** Test Message **
+            OFM alert: Data file [ {file_date} ] does not exist on GCS
+            GCS URI: {gcs_path}
+            Task: {task}
+            Dag: {dag}
+            Execution Time: {exec_date}
+            """.format(
+                task = ti.task_id,
+                dag  = ti.dag_id,
+                exec_date = ts,
+                file_date = gcs_list[-1],
+                gcs_path = '/'.join(gcs_list[0:-1]),
+            )
+
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_test',
+        http_conn_id=SLACK_OFM_ALERT_CONN,
+        webhook_token=slack_webhook_token,
+        message=slack_msg
+    )
+
     return failed_alert.execute(context=context)
