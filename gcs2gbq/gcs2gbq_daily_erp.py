18a85c4988a591fc556d332aae208218f3121ed7
puunyapat-convz <puunyapat@convz.com>
2022-04-28 18:45:30 +0700

    upate tag

--- gcs2gbq/gcs2gbq_daily_erp.py
+++ gcs2gbq/gcs2gbq_daily_erp.py
@@ -273,7 +273,7 @@ with DAG(
     start_date=dt.datetime(2022, 4, 4),
     catchup=True,
     max_active_runs=1,
-    tags=['convz_prod_airflow_style'],
+    tags=['convz', 'production', 'airflow_style', 'daily', 'erp'],
     render_template_as_native_obj=True,
     default_args={
         'on_failure_callback': ofm_task_fail_slack_alert,
