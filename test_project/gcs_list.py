1a1ffcbc3d3b8685f68c6bf27dcdbed7f1b8fc55
puunyapat-convz <puunyapat@convz.com>
2022-05-03 21:18:37 +0700

    test load only first line from GCS

--- test_project/gcs_list.py
+++ test_project/gcs_list.py
@@ -1,10 +1,56 @@
 from airflow.providers.google.cloud.operators.gcs import *
-from airflow                   import configuration, DAG
+from airflow.operators.python  import PythonOperator
+
+from airflow      import configuration, DAG
+from google.cloud import storage
 
 import datetime as dt
 
 path      = configuration.get('core','dags_folder')
 MAIN_PATH = path + "/../data"
+BLOB_CHUNK_SIZE = 2560
+
+def _get_csv_header(blob):
+
+    output = f"{MAIN_PATH}/{blob.split('/')[-1]}"
+    for line in blob_lines(blob):
+
+        with open(file = output, mode='w') as f:
+            f.write(line)
+
+        return output
+
+def blob_lines(filename):
+    position = 0
+    buff = []
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('ofm-data')
+    blob = storage.Blob(filename, bucket)
+
+    while True:
+        chunk = blob.download_as_text(
+            start=position, 
+            end=position + BLOB_CHUNK_SIZE, 
+            encoding="utf-8"
+        )
+
+        if '\n' in chunk:
+            part1, part2 = chunk.split('\n', 1)
+            buff.append(part1)
+            yield ''.join(buff)
+            parts = part2.split('\n')
+            for part in parts[:-1]:
+                yield part
+            buff = [parts[-1]]
+        else:
+            buff.append(chunk)
+
+        position += BLOB_CHUNK_SIZE + 1  # Blob chunk is downloaded using closed interval
+
+        if len(chunk) < BLOB_CHUNK_SIZE:
+            yield ''.join(buff)
+            return
+
 
 with DAG(
     dag_id="a_gcs_list",
@@ -21,12 +67,20 @@ with DAG(
     # }
 ) as dag:
 
-    list_file = GCSListObjectsOperator(
-        task_id = 'list_file',
-        bucket = 'ofm-data', 
-        prefix = 'Mercury/daily/tbproduct_content/2022_01_18', 
-        delimiter = '.jsonl',
-        gcp_conn_id = 'convz_dev_service_account'
+    # list_file = GCSListObjectsOperator(
+    #     task_id = 'list_file',
+    #     bucket = 'ofm-data', 
+    #     prefix = 'Mercury/daily/tbproduct_content/2022_01_18', 
+    #     delimiter = '.jsonl',
+    #     gcp_conn_id = 'convz_dev_service_account'
+    # )
+
+    read_sample = PythonOperator(
+        task_id=f"read_sample",
+        python_callable=_get_csv_header,
+        op_kwargs = {
+            "blob" : "Mercury/daily/tbproduct_content/2022_01_18_1642488959528_0.jsonl"
+        }
     )
 
-    list_file
+    read_sample
