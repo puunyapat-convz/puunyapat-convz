b65b1f9fa808bede58bff9ac421d8cadcb02ddd9
puunyapat-convz <puunyapat@convz.com>
2022-05-05 23:33:26 +0700

    update tag

--- gcs2gbq/intraday/gcs2gbq_intraday_tms.py
+++ gcs2gbq/intraday/gcs2gbq_intraday_tms.py
@@ -280,7 +280,7 @@ with DAG(
     # end_date=dt.datetime(2022, 4, 25, 1, 32),
     catchup=True,
     max_active_runs=1,
-    tags=['convz', 'production' ,'airflow_style', 'intraday', 'tms'],
+    tags=['convz', 'production' ,'mario', 'intraday_data', 'tms'],
     render_template_as_native_obj=True,
     default_args={
         'on_failure_callback': ofm_task_fail_slack_alert,
