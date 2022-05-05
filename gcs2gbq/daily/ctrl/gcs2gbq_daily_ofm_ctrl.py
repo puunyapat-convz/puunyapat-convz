9b755e76c0185c559b91ebecb30e257326da1b75
puunyapat-convz <puunyapat@convz.com>
2022-05-05 23:29:06 +0700

    update tag

--- gcs2gbq/daily/ctrl/gcs2gbq_daily_ofm_ctrl.py
+++ gcs2gbq/daily/ctrl/gcs2gbq_daily_ofm_ctrl.py
@@ -68,7 +68,7 @@ with DAG(
     start_date=dt.datetime(2022, 4, 28),
     catchup=True,
     max_active_runs=1,
-    tags=['convz', 'production', 'mario', 'daily', 'officemate', 'control'],
+    tags=['convz', 'production', 'mario', 'daily_ctrl', 'officemate'],
     render_template_as_native_obj=True,
     default_args={
         'on_failure_callback': ofm_task_fail_slack_alert,
