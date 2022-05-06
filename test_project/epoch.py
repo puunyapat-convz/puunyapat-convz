e4d8fdd7a7b8f67e07d732daee63b29515487c23
puunyapat-convz <puunyapat@convz.com>
2022-05-06 15:12:33 +0700

    clearn unused

--- test_project/epoch.py
+++ test_project/epoch.py
@@ -27,22 +27,4 @@ def test(**kwargs):
     if kwargs.get("pretty"):
         print('true')
 
-test( pretty = True)
-
-json_data = json.dumps({'_airbyte_ab_id': '0a3eb0d8-3222-4040-b0e6-b414430e22af',
- '_airbyte_data': {'_airbyte_arbildtl_hashid': '5039c46c754b7d977f300bd50f774274',
-                   '_airbyte_emitted_at': '2022-05-04T21:05:39.950000Z',
-                   'billdtlprvstatus': 'P',
-                   'billdtlstatus': 'P',
-                   'billno': 'BI-220501495',
-                   'createby': '9003867',
-                   'custrefno': 'TILS-P22050001',
-                   'invamt': 599.99,
-                   'invdate': '2022-05-03T00:00:00.000000Z',
-                   'lastupdateby': '9003867',
-                   'referenceno': 'SS2205000022',
-                   'referenceno1': '',
-                   'trantype': 'IV',
-                   'updateon': '2022-05-04T13:13:49.417000Z'},
- '_airbyte_emitted_at': 1651699187154}, separators=(',', ':'))
-print(json_data)
+test(pretty = True)
