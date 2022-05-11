f8c8f065773c1d65d719a5dd64cb969a06f0b67c
puunyapat-convz <puunyapat@convz.com>
2022-05-11 21:07:13 +0700

    change count file logic to comparing epoch

--- gcs2gbq/intraday/data/check_intraday_files.py
+++ gcs2gbq/intraday/data/check_intraday_files.py
@@ -1,10 +1,10 @@
-from tabnanny import check
 from airflow                   import configuration, DAG
-from airflow.operators.python  import PythonOperator, BranchPythonOperator
+from airflow.operators.python  import BranchPythonOperator
 from airflow.operators.bash    import BashOperator
 from airflow.operators.dummy   import DummyOperator
 from airflow.models            import Variable
 from airflow.utils.task_group  import TaskGroup
+from dateutil                  import parser
 from utils.dag_notification    import *
 
 import datetime as dt
@@ -15,14 +15,32 @@ path = configuration.get('core','dags_folder')
 
 MAIN_PATH   = path + "/../data"
 BUCKET_NAME = "ofm-data"
+SOURCE_TYPE = "intraday"
 
-def _count_file(source, table, filename, total, hour):
+def _count_file(ti, source, table, filename, gcs_path, total, ts):
+
+    found    = 0
+    ts_split = ts.split('T')
+
+    start_epoch = parser.parse(f"{ts_split[0]}T00:00:00.000Z").timestamp()    
+    end_epoch   = parser.parse(f"{ts}:59:59.999Z").timestamp() ## Minute must be very end of hour period of DAG interval
 
     with open(filename) as f:
         lines = f.read().splitlines()
 
-    if len(lines) < (int(total/24))*int(hour):
+    for line in lines:
+        file_epoch = int(line.split(",")[2].split("/")[-1].split("_")[3])/1000
+
+        if file_epoch >= start_epoch and file_epoch <= end_epoch:
+            found += 1
+
+    hour   = int(ts_split[1])+1
+    expect = (int(total/24))*hour
+
+    if found < expect:
         log.warning(f"Table [ {table} ] has files lower than expected.")
+        ti.xcom_push(key='gcs_uri', value=gcs_path)
+        ti.xcom_push(key='filecount', value=f"`{found}` from `{expect}`")
         return f"send_alert_{source}_{table}"
     else:
         return f"no_alert_{source}_{table}"
@@ -30,16 +48,17 @@ def _count_file(source, table, filename, total, hour):
 with DAG(
     dag_id="check_intraday_files",
     # schedule_interval=None,
-    schedule_interval="00 */8 * * *",
-    start_date=dt.datetime(2022, 4, 27),
+    schedule_interval="59 7-23/8 * * *",
+    start_date=dt.datetime(2022, 5, 10, 23, 59),
+    # end_date=dt.datetime(2022, 5, 11, 15, 59),
     catchup=True,
     max_active_runs=1,
     tags=['convz', 'production', 'mario', 'alert'],
     render_template_as_native_obj=True,
-    # default_args={
-    #     'on_failure_callback': ofm_task_fail_slack_alert,
-    #     'retries': 0
-    # }
+    default_args={
+        'on_failure_callback': ofm_task_fail_slack_alert,
+        'retries': 0
+    }
 ) as dag:
 
     start_task = DummyOperator(task_id = "start_task")
@@ -50,7 +69,7 @@ with DAG(
         default_var=['default_source'],
         deserialize_json=True
     )
-    # iterable_sources_list = { "Mercury":"24" }
+    # iterable_sources_list = { "TMS":"24" }
 
     with TaskGroup(
         'source_tasks_group',
@@ -65,7 +84,7 @@ with DAG(
                     default_var=['default_table'],
                     deserialize_json=True
                 )
-                # iterable_tables_list = [ "mercury_tbsellerorderprocesslog" ]
+                # iterable_tables_list = [ "TMS_TBDeliveryConfirm" ]
 
                 with TaskGroup(
                     f'{source_name}_tables_tasks_group',
@@ -92,12 +111,17 @@ with DAG(
                                     'source'   : source_name,
                                     'table'    : tm1_table,
                                     'filename' : f'{{{{ ti.xcom_pull(task_ids="create_list_{source_name}_{tm1_table}") }}}}',
+                                    'gcs_path' : f"gs://{BUCKET_NAME}/{source_name}/{SOURCE_TYPE}/{tm1_table}/{{{{ ds.replace('-','_') }}}}*.jsonl",
                                     'total'    : f'{{{{ var.json.intraday_alert.{source_name} }}}}',
-                                    'hour'     : '{{ ts.split("T")[1].split(":")[0] }}'
+                                    'ts'       : '{{ ts.split(":")[0] }}'
                                 },
                             )
 
-                            send_alert = DummyOperator(task_id = f"send_alert_{source_name}_{tm1_table}")
+                            send_alert = DummyOperator(
+                                task_id = f"send_alert_{source_name}_{tm1_table}",
+                                on_success_callback = ofm_missing_intraday_file_slack_alert
+                            )
+
                             no_alert   = DummyOperator(task_id = f"no_alert_{source_name}_{tm1_table}")
 
                             remove_file_list = BashOperator(
