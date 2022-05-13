07366933da6955fca6f50b4a0e9735ea96e10fe4
puunyapat-convz <puunyapat@convz.com>
2022-05-13 12:54:00 +0700

    folder rename

---
+++ gbq_intermediate/test.py
@@ -0,0 +1,17 @@
+data = {
+  "daily_ofm_tbproductwarehouse_table": "central-cto-ofm-data-hub-dev.ofm_landing_zone_views",
+
+  "potxdelt_product_detail_table": "central-cto-ofm-data-hub-prod.officemate_ofm_landing_zone_views",
+  "product_warehouse_daily_table": "central-cto-ofm-data-hub-prod.officemate_ofm_landing_zone_views",
+  "productwarehouse_product_detail_table": "central-cto-ofm-data-hub-prod.officemate_ofm_landing_zone_views",
+  "soivdelt_product_detail_table": "central-cto-ofm-data-hub-prod.officemate_ofm_landing_zone_views",
+  "allocate_stock_bychannel_daily_table": "central-cto-ofm-data-hub-prod.officemate_ofm_landing_zone_views",
+
+  "tbrddetail_product_detail_table": "central-cto-ofm-data-hub-prod.erp_ofm_landing_zone_views",
+  "tbreceivedetail_product_detail_table": "central-cto-ofm-data-hub-prod.erp_ofm_landing_zone_views",
+  "tbrtdelt_product_detail_table": "central-cto-ofm-data-hub-prod.erp_ofm_landing_zone_views",
+  "tbrtvdetail_product_detail_table": "central-cto-ofm-data-hub-prod.erp_ofm_landing_zone_views",
+  "tbwtdetail_product_detail_table": "central-cto-ofm-data-hub-prod.erp_ofm_landing_zone_views"
+}
+
+print(len(data))
