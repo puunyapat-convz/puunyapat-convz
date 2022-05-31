7cea5347ac00741dbbe5d8378169525c02a18bd5
puunyapat-convz <puunyapat@convz.com>
2022-05-31 20:07:48 +0700

    add new alert message

--- gcs2gbq/utils/dag_notification.py
+++ gcs2gbq/utils/dag_notification.py
@@ -125,3 +125,42 @@ OFM alert: *Intraday data files* with prefix `{gcs_prefix}` has lower count than
         message=slack_msg
     )
     return failed_alert.execute(context=context)
+
+def ofm_stuck_sftp_file_slack_alert(context):
+    slack_webhook_token  = BaseHook.get_connection(SLACK_OFM_ALERT_CONN).password
+
+    ti = context.get('task_instance')
+    ts = context.get('ts')
+
+    task_suffix = '_'.join(ti.task_id.split('_')[2:])
+    stuck_files = ti.xcom_pull(key='stuck_files', task_ids=f'list_file_{task_suffix}')
+    sftp_path   = ti.xcom_pull(key='sftp_path', task_ids=f'list_file_{task_suffix}')
+
+    source_name = '_'.join(ti.task_id.split('_')[2:4])
+    table_name  = '_'.join(ti.task_id.split('_')[4:])
+    print(f'source name = {source_name}')
+
+    slack_msg = """
+*** Test message, please ignore ***
+OFM alert: Found `{file_count}` *unprocess files* from source `{source}`, table `{table}` on SFTP.
+>DAG: {dag}
+>Task: {task}
+>SFTP Path: {path_name}
+>Execution Time: {exec_date}
+            """.format(
+                task  = ti.task_id,
+                dag   = ti.dag_id,
+                source = source_name,
+                table  = table_name,
+                exec_date  = ts,
+                file_count = len(stuck_files),
+                path_name  = sftp_path
+            )
+
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_test',
+        http_conn_id=SLACK_OFM_ALERT_CONN,
+        webhook_token=slack_webhook_token,
+        message=slack_msg
+    )
+    return failed_alert.execute(context=context)
