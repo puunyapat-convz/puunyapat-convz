d954398baa26382762700200b2f5ccfa57627acc
puunyapat-convz <puunyapat@convz.com>
2022-06-01 23:14:25 +0700

    update tag

--- gbq_intermediate/gbq_daily_intermediate.py
+++ gbq_intermediate/gbq_daily_intermediate.py
@@ -43,7 +43,7 @@ with DAG(
     start_date=dt.datetime(2022, 5, 11),
     catchup=True,
     max_active_runs=1,
-    tags=['convz', 'production', 'mario', 'daily_view', 'intermediate'],
+    tags=['convz', 'production', 'mario', 'intermediate'],
     render_template_as_native_obj=True,
     default_args={
         'on_failure_callback': ofm_task_fail_slack_alert,
