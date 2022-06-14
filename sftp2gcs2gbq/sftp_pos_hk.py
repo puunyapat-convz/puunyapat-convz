48e54658bd65862921e07fcd30c1774d6eeda4de
puunyapat-convz <puunyapat@convz.com>
2022-06-14 19:46:01 +0700

    change to gsutil

--- sftp2gcs2gbq/sftp_pos_hk.py
+++ sftp2gcs2gbq/sftp_pos_hk.py
@@ -2,6 +2,7 @@ from shutil import move
 from airflow                   import configuration, DAG
 from airflow.operators.python  import PythonOperator, BranchPythonOperator
 from airflow.operators.dummy   import DummyOperator
+from airflow.operators.bash    import BashOperator
 from airflow.models            import Variable
 from airflow.utils.task_group  import TaskGroup
 from airflow.macros            import *
@@ -10,7 +11,7 @@ from utils.dag_notification    import *
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import *
 
 import datetime as dt
-import logging
+import logging, os
 
 ######### VARIABLES ###########
 
@@ -25,13 +26,27 @@ def _gen_date(ds, offset):
     ds_nodash = ds_add(ds, offset).replace('-','')
     return [ ds_add(ds, offset), f'{ds_nodash[0:4]}/{ds_nodash[0:6]}/' ]
 
+def _check_file(filename, tablename, branch_id):
+
+    with open(filename) as f:
+        count = sum(1 for _ in f)
+
+    if count == 0:
+        log.info(f"Table [ {tablename} ] has no file(s) for this run.")
+        task_id = f"skip_table_{branch_id}"
+    else:
+        task_id = f"move_file_{branch_id}"
+
+    os.remove(filename)
+    return task_id    
+
 with DAG(
     dag_id="sftp_pos_hk",
-    schedule_interval=None,
-    # schedule_interval="00 14 * * *",
-    start_date=dt.datetime(2022, 2, 28),
-    # end_date=dt.datetime(2022, 3, 8),
-    catchup=False,
+    # schedule_interval=None,
+    schedule_interval="00 14 * * *",
+    start_date=dt.datetime(2022, 3, 7),
+    end_date=dt.datetime(2022, 4, 7),
+    catchup=True,
     max_active_runs=1,
     tags=['convz', 'production', 'mario', 'sftp', 'utility'],
     render_template_as_native_obj=True,
@@ -61,7 +76,7 @@ with DAG(
     ) as gcs_hk_tasks_group:
 
         for BUCKET_NAME in iterable_sources_list.keys():           
-            source = "-".join(BUCKET_NAME.split('-')[1:3])
+            source = "_".join(BUCKET_NAME.split('-')[1:3])
 
             start_source = DummyOperator(task_id = f"start_{source}")
 
@@ -72,7 +87,8 @@ with DAG(
 
                 for BUCKET_PATH in iterable_sources_list.get(BUCKET_NAME).keys():
 
-                    path_id = f"{source}-" + "-".join(BUCKET_PATH.split('_')[-2::1])
+                    path_id   = f"{source}_" + "_".join(BUCKET_PATH.split('_')[-2::1])
+                    tablename = BUCKET_PATH.split('/')[-1]
                     MIN_DATE, MAX_DATE = iterable_sources_list.get(BUCKET_NAME).get(BUCKET_PATH)
 
                     with TaskGroup(
@@ -85,7 +101,7 @@ with DAG(
                             int_id = "{:02d}".format(abs(interval))
 
                             gen_date = PythonOperator(
-                                task_id = f"gen_date_{path_id}-{int_id}",
+                                task_id = f"gen_date_{path_id}_{int_id}",
                                 python_callable = _gen_date,
                                 op_kwargs = {
                                     "ds"    : '{{ data_interval_end.strftime("%Y-%m-%d") }}',
@@ -93,27 +109,34 @@ with DAG(
                                 }
                             )
 
-                            with TaskGroup(
-                                f'move_{path_id}_{interval}_tasks_group',
-                                prefix_group_id=False,
-                            ) as move_ext_tasks_group:
+                            create_list = BashOperator(
+                                task_id = f"create_list_{path_id}_{int_id}",
+                                cwd     = MAIN_PATH,
+                                bash_command = f'temp=$(mktemp {path_id}_{int_id}.XXXXXXXX)'
+                                                + f' && gsutil ls "gs://{BUCKET_NAME}/{BUCKET_PATH}/*_{{{{ ti.xcom_pull(task_ids="gen_date_{path_id}_{int_id}")[0] }}}}.*"'
+                                                + f' > $temp; echo {MAIN_PATH}/$temp'
+                            )
 
-                                for type in FILE_TYPE:
+                            check_list = BranchPythonOperator(
+                                task_id=f'check_list_{path_id}_{int_id}',
+                                python_callable=_check_file,
+                                op_kwargs = { 
+                                    'branch_id': f"{path_id}_{int_id}",
+                                    'tablename': tablename,
+                                    'filename' : f'{{{{ ti.xcom_pull(task_ids="create_list_{path_id}_{int_id}") }}}}',
+                                }
+                            )
 
-                                    move_file = GCSToGCSOperator(
-                                        task_id = f"move_{type}_{path_id}-{int_id}",
-                                        source_bucket = BUCKET_NAME,
-                                        source_object = BUCKET_PATH + f'/*_{{{{ ti.xcom_pull(task_ids="gen_date_{path_id}-{int_id}")[0] }}}}.{type}',
-                                        # source_object=BUCKET_PATH + '/BI',
-                                        # delimiter = f'_{{{{ ti.xcom_pull(task_ids="gen_date_{path_id}-{int_id}")[0] }}}}.{type}',
-                                        destination_bucket = None, 
-                                        destination_object = BUCKET_PATH + f'/{{{{ ti.xcom_pull(task_ids="gen_date_{path_id}-{int_id}")[1] }}}}',
-                                        gcp_conn_id ='convz_dev_service_account',
-                                        move_object = False, 
-                                        replace = True
-                                    )
+                            move_file = BashOperator(
+                                task_id = f"move_file_{path_id}_{int_id}",
+                                cwd     = MAIN_PATH,
+                                bash_command = f'gsutil -m mv "gs://{BUCKET_NAME}/{BUCKET_PATH}/*_{{{{ ti.xcom_pull(task_ids="gen_date_{path_id}_{int_id}")[0] }}}}.*"'
+                                                 + f' gs://{BUCKET_NAME}/{BUCKET_PATH}/{{{{ ti.xcom_pull(task_ids="gen_date_{path_id}_{int_id}")[1] }}}}'
+                            )
 
-                                    gen_date >> move_file
+                            skip_table = DummyOperator(task_id = f"skip_table_{path_id}_{int_id}")
+
+                            gen_date >> create_list >> check_list >> [ move_file, skip_table ]
 
             start_source >> move_bucket_tasks_group
 
