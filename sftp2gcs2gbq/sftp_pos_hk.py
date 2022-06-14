e269f70af443495afa8d3883d89ce674fe5b286d
puunyapat-convz <puunyapat@convz.com>
2022-06-14 20:28:18 +0700

    change end_date

--- sftp2gcs2gbq/sftp_pos_hk.py
+++ sftp2gcs2gbq/sftp_pos_hk.py
@@ -45,7 +45,7 @@ with DAG(
     # schedule_interval=None,
     schedule_interval="00 14 * * *",
     start_date=dt.datetime(2022, 3, 7),
-    end_date=dt.datetime(2022, 4, 7),
+    end_date=dt.datetime(2022, 6, 6),
     catchup=True,
     max_active_runs=1,
     tags=['convz', 'production', 'mario', 'sftp', 'utility'],
