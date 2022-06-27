8881d01786c418eb342b89f43ec5c8e0578b1484
puunyapat-convz <puunyapat@convz.com>
2022-06-27 19:22:55 +0700

    first version

---
+++ test_project/build_query.py
@@ -0,0 +1,106 @@
+test= [
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbrddetail_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbrddetail"],
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbreceivedetail_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbreceivedetail"],
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbreceivehead_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbreceivehead"],
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbrtdetail_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbrtdetail"],
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbrthead_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbrthead"],
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbrtvdetail_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbrtvdetail"],
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbrtvhead_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbrtvhead"],
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbwtdetail_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbwtdetail"],
+    ["central-cto-ofm-data-hub-prod.erp_ofm_daily.tbwthead_daily_source","central-cto-ofm-data-hub-dev.ERP_source.daily_tbwthead"],
+    ["central-cto-ofm-data-hub-prod.jda_b2s_daily_source.BCH_JDA_DataPlatform_JDAACSTK","central-cto-ofm-data-hub-dev.b2s_jda_prod_new.BCH_JDA_DataPlatform_JDAACSTK"],
+    ["central-cto-ofm-data-hub-prod.jda_b2s_daily_source.BCH_JDA_DataPlatform_JDASAL","central-cto-ofm-data-hub-dev.b2s_jda_prod_new.BCH_JDA_DataPlatform_JDASAL"],
+    ["central-cto-ofm-data-hub-prod.jda_b2s_daily_source.BCH_JDA_DataPlatform_JDASTK","central-cto-ofm-data-hub-dev.b2s_jda_prod_new.BCH_JDA_DataPlatform_JDASTK"],
+    ["central-cto-ofm-data-hub-prod.jda_b2s_daily_source.BCH_JDA_DataPlatform_JDATRN","central-cto-ofm-data-hub-dev.b2s_jda_prod_new.BCH_JDA_DataPlatform_JDATRN"],
+    ["central-cto-ofm-data-hub-prod.jda_b2s_daily_source.BCH_JDA_DataPlatform_MGRBVD","central-cto-ofm-data-hub-dev.b2s_jda_prod_new.BCH_JDA_DataPlatform_MGRBVD"],
+    ["central-cto-ofm-data-hub-prod.jda_b2s_daily_source.BCH_JDA_DataPlatform_MGROBS1","central-cto-ofm-data-hub-dev.b2s_jda_prod_new.BCH_JDA_DataPlatform_MGROBS1"],
+    ["central-cto-ofm-data-hub-prod.jda_ofm_daily_source.BCH_JDA_DataPlatform_JDASAL","central-cto-ofm-data-hub-dev.ofm_jda_prod_new.BCH_JDA_DataPlatform_JDASAL"],
+    ["central-cto-ofm-data-hub-prod.jda_ofm_daily_source.BCH_JDA_DataPlatform_JDASTK","central-cto-ofm-data-hub-dev.ofm_jda_prod_new.BCH_JDA_DataPlatform_JDASTK"],
+    ["central-cto-ofm-data-hub-prod.jda_ofm_daily_source.BCH_JDA_DataPlatform_JDATRN","central-cto-ofm-data-hub-dev.ofm_jda_prod_new.BCH_JDA_DataPlatform_JDATRN"],
+    ["central-cto-ofm-data-hub-prod.jda_ofm_daily_source.BCH_JDA_DataPlatform_MGRBVD","central-cto-ofm-data-hub-dev.ofm_jda_prod_new.BCH_JDA_DataPlatform_JDATRN"],
+    ["central-cto-ofm-data-hub-prod.jda_ofm_daily_source.BCH_JDA_DataPlatform_MGROBS1","central-cto-ofm-data-hub-dev.ofm_jda_prod_new.BCH_JDA_DataPlatform_MGROBS1"],
+    ["central-cto-ofm-data-hub-prod.mercury_ofm_intraday.mercury_tborder_intraday_source","central-cto-ofm-data-hub-dev.Mercury_source_intraday.intraday_mercury_tborder"],
+    ["central-cto-ofm-data-hub-prod.officemate_ofm_daily.ofm_soivhead_daily_source","central-cto-ofm-data-hub-dev.officemate_source.daily_ofm_soivhead"],
+    ["central-cto-ofm-data-hub-prod.officemate_ofm_daily.ofm_tbproductwarehouse_daily_source","central-cto-ofm-data-hub-dev.officemate_source.daily_ofm_tbproductwarehouse"],
+    ["central-cto-ofm-data-hub-prod.officemate_ofm_daily.ofm_tbso_detail_daily_source","central-cto-ofm-data-hub-dev.officemate_source.daily_ofm_tbso_detail"],
+    ["central-cto-ofm-data-hub-prod.officemate_ofm_daily.potxdetl_daily_source","central-cto-ofm-data-hub-dev.officemate_source.daily_potxdetl"],
+    ["central-cto-ofm-data-hub-prod.officemate_ofm_daily.potxhead_daily_source","central-cto-ofm-data-hub-dev.officemate_source.daily_potxhead"],
+    ["central-cto-ofm-data-hub-prod.officemate_ofm_daily.tbobs_nonstore_monthly_daily_source","central-cto-ofm-data-hub-dev.officemate_source.daily_ofm_tbobs_nonstore_monthly"],
+    ["central-cto-ofm-data-hub-prod.officemate_ofm_historical.tbproductwarehouse_omthistory_historical_source","central-cto-ofm-data-hub-dev.officemate_source.daily_tbproductwarehouse_omthistory"],
+    ["central-cto-ofm-data-hub-prod.pos_b2s_daily_source.POS_DataPlatform_Txn_Payment","central-cto-ofm-data-hub-dev.b2s_pos_prod.POS_DataPlatform_Txn_Payment"],
+    ["central-cto-ofm-data-hub-prod.pos_b2s_daily_source.POS_DataPlatform_Txn_Sales","central-cto-ofm-data-hub-dev.b2s_pos_prod.POS_DataPlatform_Txn_Sales"],
+    ["central-cto-ofm-data-hub-prod.pos_ofm_daily_source.POS_DataPlatform_Txn_Payment","central-cto-ofm-data-hub-dev.ofm_pos_prod.POS_DataPlatform_Txn_Payment"],
+    ["central-cto-ofm-data-hub-prod.pos_ofm_daily_source.POS_DataPlatform_Txn_Sales","central-cto-ofm-data-hub-dev.ofm_pos_prod.POS_DataPlatform_Txn_Sales"]
+]
+
+SQL_COMMAND = { 
+    "COUNT"  : 
+"""SELECT
+    [partition] AS `create_date`,
+    COUNT(*) AS `count`
+FROM
+    `[fqdn]`
+GROUP BY
+    1
+ORDER BY
+    1""",
+                        
+    "COMPARE": 
+"""SELECT
+    *,
+    `count_dev`-`count_prod` AS diff,
+    `count_dev`/`count_prod` AS dup
+FROM (
+    SELECT
+        [partition] AS date_dev,
+        COUNT(*) AS count_dev
+    FROM
+        `[dev_fqdn]`
+    GROUP BY
+        1)
+    FULL OUTER JOIN (
+    SELECT
+        [partition] AS date_prod,
+        COUNT(*) AS count_prod
+    FROM
+        `[prod_fqdn]`
+    GROUP BY
+        1)
+ON
+    date_prod = date_dev
+ORDER BY
+    date_prod"""
+}
+
+for table in test:
+    PRD_FQDN, DEV_FQDN = table
+    dataset = []
+    table   = ""
+
+    if PRD_FQDN != "": 
+        PRD_PROJECT, PRD_DATASET, PRD_TABLE = PRD_FQDN.split(".")
+        table = PRD_TABLE
+        dataset.append(PRD_DATASET)
+
+    if DEV_FQDN != "":
+        DEV_PROJECT, DEV_DATASET, DEV_TABLE = DEV_FQDN.split(".")
+        if table == "": table = DEV_TABLE 
+        dataset.append(DEV_DATASET)  
+
+    for ds in dataset:
+        if "pos" in ds or "jda" in ds:
+            partition = "DATE(_PARTITIONTIME)"
+        else:
+            partition = "`report_date`"
+
+    if PRD_FQDN != "" and DEV_FQDN != "":
+        query = SQL_COMMAND.get("COMPARE").replace("[partition]",partition)
+        query = query.replace("[dev_fqdn]",DEV_FQDN).replace("[prod_fqdn]",PRD_FQDN)
+    else:
+        fdqn = PRD_FQDN if PRD_FQDN != "" else DEV_FQDN
+        query = SQL_COMMAND.get("COUNT").replace("[partition]",partition)
+        query = query.replace("[fqdn]",fdqn)
+
+    print(table)
+    # print(query)
+    print()
