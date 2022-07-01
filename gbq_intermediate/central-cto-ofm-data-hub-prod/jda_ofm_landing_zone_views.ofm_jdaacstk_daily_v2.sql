337332e87265bc8e107e79f0e871a9cc6c722425
puunyapat-convz <puunyapat@convz.com>
2022-07-01 17:30:48 +0700

    first version

---
+++ gbq_intermediate/central-cto-ofm-data-hub-prod/jda_ofm_landing_zone_views.ofm_jdaacstk_daily_v2.sql
@@ -0,0 +1,17 @@
+WITH
+  tmp AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY INSTOR, INSKU ORDER BY INTRND DESC, DATE(_PARTITIONTIME) DESC) rnk,
+    DATE(_PARTITIONTIME) AS partition_date
+  FROM
+    `central-cto-ofm-data-hub-prod.jda_ofm_daily_source.BCH_JDA_DataPlatform_JDAACSTK`
+  WHERE
+    INTRND <= CURRENT_DATE )
+SELECT
+  * EXCEPT (rnk,
+    partition_date)
+FROM
+  tmp
+WHERE
+  rnk =1
