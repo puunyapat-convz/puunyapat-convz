07e3d56a3a7eb8a3bd06799714aa8fc77864de27
puunyapat-convz <puunyapat@convz.com>
2022-07-06 13:55:24 +0700

    change schedule

--- sftp2gcs2gbq/check_sftp_files.py
+++ sftp2gcs2gbq/check_sftp_files.py
@@ -37,8 +37,8 @@ def _list_file(ti, hookname, mainfolder, subfolder, tablename):
 with DAG(
     dag_id="check_sftp_files",
     # schedule_interval=None,
-    schedule_interval="30 02 * * *",
-    start_date=dt.datetime(2022, 5, 31),
+    schedule_interval="30 18 * * *",
+    start_date=dt.datetime(2022, 7, 5),
     catchup=True,
     max_active_runs=1,
     tags=['convz', 'production', 'mario', 'alert', 'sftp'],
